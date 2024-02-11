e734cae7e36e15bc61f4a530e9ec18a15f7321e8
Bruno <dolareeu@gmail.com>
2024-02-11 17:57:04 -0300

    model do ingrediente e redirecionamento para o cadastro do ingrediente

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,8 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
+import model.ModelIngrediente;
 
-@WebServlet(urlPatterns= {"/Controller","/lanches"})
+@WebServlet(urlPatterns= {"/Controller","/lanches","/insertIngre"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO con = new DAO();
@@ -21,10 +22,19 @@ public class Controller extends HttpServlet {
 		String action = request.getServletPath();
 		if(action.equals("/lanches")) {
 			this.lanches(request, response);
+		}else if(action.equals("/insertIngre")){
+			novoIngre(request,response);
+		}else {
+			response.sendRedirect("index.hmtl");
 		}
 	}
 	protected void lanches(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendRedirect("Lanches.jsp");
+	}
+	
+	protected void novoIngre(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ModelIngrediente ingre = new ModelIngrediente();
+		ingre.setNome(request.getParameter("ingrediente"));
 	}
 
 }
